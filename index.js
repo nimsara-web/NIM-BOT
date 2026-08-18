60927ac32ca2e12440ff960fcac76be7920dbaea
Nimsara <nimsaranethmintha@gmail.com>
2026-08-18 14:29:24 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '/')));
+
+const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://username:password@cluster.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("📦 Connected to MongoDB successfully!");
+}).catch((err) => {
+    console.error("❌ MongoDB connection error:", err);
+});
+
+const pairRouter = require('./pair');
+app.use('/pair', pairRouter);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'main.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`🚀 NIM BOT Server is running on port ${PORT}`);
+});
+
+module.exports = app;
